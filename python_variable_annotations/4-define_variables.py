8785637199265aeaba35d6b513e80a436b07faea
cousinfabien <cousin.fabien3@gmail.com>
2026-04-07 13:44:30 +0200

    Add task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module demonstrates variable annotations in Python.
+Several variables are defined with specific types and values.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
