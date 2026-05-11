c112e96f2fb15bb85a1d7ab1bee25103db4ae5e1
cousinfabien <cousin.fabien3@gmail.com>
2026-05-11 12:01:22 +0000

    Add task 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    if (lines.length <= 1) {
+      console.log('Number of students: 0');
+      return;
+    }
+    lines.shift();
+    const students = lines;
+    console.log(`Number of students: ${students.length}`);
+    const fields = {};
+
+    for (const student of students) {
+      const studentData = student.split(',');
+      const firstName = studentData[0];
+      const field = studentData[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    }
+    for (const [field, list] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${list.length}. List:${list.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
