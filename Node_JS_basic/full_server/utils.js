ec896e8317e0eb7bee36f99a2fbd31fe76ec5c09
cousinfabien <cousin.fabien3@gmail.com>
2026-05-12 12:42:24 +0000

    Add Task 8 : utils.js file

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    const lines = data.split('\n').filter((line) => line.trin() !== '');
+    const students = lines.slice(1);
+    const fields = {};
+
+    for (const student of students) {
+      const studentData = student.split(',');
+      if (studentData.length >= 4) {
+        const firstName = studentData[0];
+        const field = studentData[3].trim();
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstName);
+      }
+    }
+    resolve(fields);
+  });
+});
+
+export default readDatabase;
